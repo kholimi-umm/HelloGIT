8b670119ec57cb465c63f0fce8ccaeb992e83868
kholimi-umm <68373977+kholimi-umm@users.noreply.github.com>
2022-03-08 15:21:59 +0700

    Update ClassHelloGIT.java

--- src/ClassHelloGIT.java
+++ src/ClassHelloGIT.java
@@ -4,6 +4,7 @@ public class ClassHelloGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GIT");
+		System.out.println("Hello GIT, update from Github");
 
 	}
 
